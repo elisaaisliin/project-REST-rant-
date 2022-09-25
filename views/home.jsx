d379039c52080f721ce94edf6e2e27012358eea1
elisaaisliin <elisadecasas1@gmail.com>
2022-09-25 09:50:29 -0700

    adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>REST-Rant</h1>
               <div>
                 <img src="/images/sushi-rolls.jpg" alt="sushi rolls" />
                 <div>
