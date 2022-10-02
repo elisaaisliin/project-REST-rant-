16e5954b6e780f159a0679ddca8b0091d7030cd0
elisaaisliin <elisadecasas1@gmail.com>
2022-10-02 00:09:37 -0700

    added edit functionality and some css

--- views/default.jsx
+++ views/default.jsx
@@ -2,30 +2,38 @@ const React = require('react')
 
 function Def (html) {
     return (
-        <html>
-            <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
-                <link rel="stylesheet" href="/css/style.css"/>
-            </head>
-            <body>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="/places">Places</a>
-                        </li>
-                        <li>
-                            <a href="/places/new">New Place</a>
-                        </li>
-                    </ul>
-                </nav>
-                {html.children}
-            </body>
-        </html>
-    )
+      <html>
+        <head>
+          <title>Title</title>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+          />
+          <link rel="stylesheet" href="/css/style.css" />
+        </head>
+        <body>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/places">Places</a>
+              </li>
+              <li>
+                <a href="/places/new">New Place</a>
+              </li>
+            </ul>
+          </nav>
+          {html.children}
+          <footer class="footer mt-auto py-3 bg-">
+            <div class="container">
+              <span class="text-muted">elisa de casas</span>
+            </div>
+          </footer>
+        </body>
+      </html>
+    );
   }
   
 
