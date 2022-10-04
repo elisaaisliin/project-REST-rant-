8acd5d46989516d82fa53cfb2a7fa5dc04ed6ff5
elisaaisliin <elisadecasas1@gmail.com>
2022-10-03 23:29:12 -0700

    added year validations

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,9 +7,14 @@ function new_form() {
       <main>
         <h1>Add a New Place</h1>
         <form method="POST" action="/places">
-          <div className="form-group">
-            <label for="founded">Founded Year</label>
-            <input className="form-control" id="founded" name="founded" />
+          <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded Year</label>
+            <input
+              type="number"
+              className="form-control"
+              id="founded"
+              name="founded"
+              value={new Date().getFullYear()} />
           </div>
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
